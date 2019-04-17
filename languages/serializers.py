30c0b1726cc5f1a7443d2da4b1b4a5bb2132bca6
Nirmal vekariya <n9879970008@gmail.com>
2019-04-18 01:37:50 +0530

    Change

--- languages/serializers.py
+++ languages/serializers.py
@@ -4,4 +4,4 @@ from .models import Language
 class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
-        fields = ('id', 'tname', 'tdesc')
+        fields = ('id', 'name', 'paradigm')
